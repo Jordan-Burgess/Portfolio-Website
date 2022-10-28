e3a2f2b5b962313d6cb2b378b8184d58f3d8fa32
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-28 10:25:48 -0400

    Add project data and import to project page

---
+++ src/allProjects.js
@@ -0,0 +1,37 @@
+import myBooksy from './images/mybooksy.png';
+import polyU from './images/polyu.png';
+import solitaire from './images/solitaire.png';
+import spielBlog from './images/spielblog.png';
+
+const projects = [
+    {
+        name: "PolyU",
+        image: polyU,
+        tech: [],
+        github: "https://github.com/Jordan-Burgess/PolyU",
+        live: "https://poly-u.netlify.app/",
+    },
+    {
+        name: "MyBooksy",
+        image: myBooksy,
+        tech: [],
+        github: "https://github.com/Jordan-Burgess/mybooksy-frontend",
+        live: "https://6336fb495211cf00744988d7--cheery-twilight-75bceb.netlify.app/",
+    },
+    {
+        name: "Spiel Blog",
+        image: spielBlog,
+        tech: [],
+        github: "https://github.com/Jordan-Burgess/Spiel-Blog-App",
+        live: "https://spiel-blog.herokuapp.com/",
+    },
+    {
+        name: "Solitaire",
+        image: solitaire,
+        tech: [],
+        github: "https://github.com/Jordan-Burgess/Solitaire",
+        live: "https://jordan-burgess.github.io/Solitaire/",
+    },
+];
+
+export default projects
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import projects from '../allProjects'
 
 export default function Projects() {
   return (
