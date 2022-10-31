80fc364373bd429a6b73e2b391776d58110326de
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-31 14:19:27 -0400

    Update logos and metadata

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from '../images/logo-no-background.png'
 
 export default function Nav() {
   return (
     <nav className='navbar fixed-top navbar-expand-lg bg-light' id='main-nav'>
       <div class="container-fluid">
-      <a className='navbar-brand' href='#home'>Jordan Burgess</a>
+      <a className='navbar-brand' href='#home'><img className='logo' src={logo} alt='logo'/></a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
