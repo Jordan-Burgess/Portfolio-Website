02fa3588ea49f9dcfe6ae4434322371925cccb5d
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-31 18:02:04 -0400

    Update logo sizes and spacing

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -14,7 +14,7 @@ export default function Contact() {
           <p>E-mail: alfredjordan93@gmail.com</p>
         </div>
         <div>
-          <a href='https://github.com/Jordan-Burgess' target="_blank" rel="noopener noreferrer"><img src={github} alt='github' className='contactlogo'/></a>
+          <a href='https://github.com/Jordan-Burgess' target="_blank" rel="noopener noreferrer"><img src={github} alt='github' className='contactlogo git'/></a>
           <a href='https://www.linkedin.com/in/jordanburgess-ny/' target="_blank" rel="noopener noreferrer"><img src={linkedin} alt='linkedin' className='contactlogo'/></a>
         </div>
 
