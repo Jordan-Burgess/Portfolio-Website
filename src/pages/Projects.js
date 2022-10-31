b978818f725a62398ed7b934877dd752726dd3d7
Jordan-Burgess <alfredjordan93@gmail.com>
2022-10-31 17:32:02 -0400

    Update project data to include description and tech

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,13 +4,15 @@ import projects from '../allProjects'
 export default function Projects() {
   return (
     <div id='projects' className='Page'>
-        <h1>Projects</h1>
+        <h1>My Projects</h1>
         <div className='AllProjects'>
         {projects.map((project) => {
             return (
                 <div className='ProjectCard'>
                   <h2>{project.name}</h2>
                   <img src={project.image} alt='Project' className='ProjectImage'/>
+                  <p className='description'>{project.description}</p>
+                  <p>Tech Used: {project.tech.join(' | ')}</p>
                   <div className='links'>
                     <a href={project.github}>GitHub</a>
                     <a href={project.live}>Live Site</a>
