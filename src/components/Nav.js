624747e6c3823b5239d33ef0c5901d98237f2a1d
Jordan-Burgess <alfredjordan93@gmail.com>
2023-11-07 21:19:01 -0500

    Change class attribute to className

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,13 +5,13 @@ import resume from '../images/resume.pdf'
 export default function Nav() {
   return (
     <nav className='navbar navbar-dark fixed-top navbar-expand-lg bg-dark' id='main-nav'>
-      <div class="container-fluid">
+      <div className="container-fluid">
       <a className='navbar-brand' href='#home'><img className='logo' src={logo} alt='logo'/></a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <div class="navbar-nav">
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
           <a className='nav-link' href='#about'>About</a>
           <a className='nav-link' href='#projects'>Projects</a>
           <a className='nav-link' href='#contact'>Contact</a>
